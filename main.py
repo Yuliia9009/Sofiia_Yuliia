c59da85c7e1bd647b657dd729a9eb26226fcd39d
xiaomiyao <130556719+xiaomiyao@users.noreply.github.com>
2024-02-17 15:51:40 +0100

    Add files via upload
    added game files

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("Hello Sofiia, Let's play!")
-print("Hello Yuliia, Let's play!")
-print("test")
+from GameBoard import GameBoard
+
+game_board = GameBoard()
+
+# Game loop
+game_board.print_game_map()
+while True:
+    direction = input("Move (up, down, left, right): ").lower()
+    if direction in ["up", "down", "left", "right"]:
+        game_board.move_character(direction)
+        game_board.move_enemy_towards_player()
+        game_board.print_game_map()
+        game_board.check_for_events()
+    else:
+        print("Invalid direction. Please choose 'up', 'down', 'left', or 'right'.")
+
+
+
+
